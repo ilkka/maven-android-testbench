7044321577768a62f6e407e4763ac2e32d742c6c
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-19 19:03:44 +0200

    Do a http request to test the tests

--- src/main/java/com/ilkka/HelloAndroidActivity.java
+++ src/main/java/com/ilkka/HelloAndroidActivity.java
@@ -3,23 +3,39 @@ package com.ilkka;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.net.http.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.message.*;
+import org.apache.http.*;
 
 public class HelloAndroidActivity extends Activity {
 
-    private static String TAG = "testbench";
+	private static String TAG = "testbench";
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	private AndroidHttpClient mClient;
+
+	/**
+	 * Called when the activity is first created.
+	 * @param savedInstanceState If the activity is being re-initialized after 
+	 * previously being shut down then this Bundle contains the data it most 
+	 * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
+		setContentView(R.layout.main);
+		if (mClient != null) {
+			mClient.execute(new HttpGet("http://www.example.com"));
+		}
+	}
 
+	/**
+	 * Set HTTP client instance.
+	 * @param client the client.
+	 */
+	public void setHttpClient(AndroidHttpClient client) {
+		mClient = client;
+	}
 }
 
