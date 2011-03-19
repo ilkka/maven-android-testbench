2b352cd8b442bb8cc31e4f835197a7bb0a22c76d
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-19 20:14:56 +0200

    Verify http call and response return code

--- src/test/java/com/ilkka/HelloAndroidActivityTest.java
+++ src/test/java/com/ilkka/HelloAndroidActivityTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.IsNot.*;
 import static org.hamcrest.core.IsEqual.*;
+import static org.hamcrest.core.IsNull.*;
 
 import static org.mockito.Mockito.*;
 
@@ -20,11 +21,16 @@ import org.apache.http.*;
 public class HelloAndroidActivityTest {
 
 	@Test
-	public void trueShouldNotBeFalse() throws Exception {
+	public void shouldGetStuffFromInternet() throws Exception {
 		AndroidHttpClient mockClient = mock(AndroidHttpClient.class);
-		when(mockClient.execute(new HttpGet("http://www.example.com/"))).thenReturn(new BasicHttpResponse(new ProtocolVersion("HTTP", 1, 1), 200, "OK"));
+		when(mockClient.execute(any(HttpGet.class))).thenReturn(new BasicHttpResponse(new ProtocolVersion("HTTP", 1, 1), 200, "OK"));
+
 		HelloAndroidActivity activity = new HelloAndroidActivity();
 		activity.setHttpClient(mockClient);
 		activity.onCreate(null);
+
+		verify(mockClient).execute(any(HttpUriRequest.class));
+		assertThat(activity.response(), notNullValue());
+		assertEquals(activity.response().getStatusLine().getStatusCode(), 200);
 	}
 }
